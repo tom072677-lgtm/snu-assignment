940f5ef76512ee00d2c563623e6790d76702e0a2
tom072677-lgtm <cys2727@snu.ac.kr>
2026-04-20 21:21:04 +0900

    skipWaiting 추가 및 캐시 v3

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open("assignment-app-v3").then((cache) => {
+    caches.open("assignment-app-v4").then((cache) => {
       return cache.addAll([
         "./",
         "./index.html",
