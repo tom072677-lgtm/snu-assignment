271ee64103ff3e4c87d6fcf82274d2004eec78c3
tom072677-lgtm <cys2727@snu.ac.kr>
2026-05-12 02:09:19 +0900

    feat: 푸시 알림(24h/5h/1h) + 다크 모드 추가

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open("assignment-app-v9").then((cache) => {
+    caches.open("assignment-app-v10").then((cache) => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -18,7 +18,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== "assignment-app-v9").map((k) => caches.delete(k)))
+      Promise.all(keys.filter((k) => k !== "assignment-app-v10").map((k) => caches.delete(k)))
     ).then(() => clients.claim())
   );
 });
